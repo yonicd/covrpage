d6a68f8e3980cc75206ed450fed45042551e2035
Jonathan Sidi <yonicd@gmail.com>
2018-05-01 16:15:21 -0400

    update travis test

--- tests/testthat/test-covrpage.R
+++ tests/testthat/test-covrpage.R
@@ -39,19 +39,21 @@ test_that("check on remote repo",{
 # })
 
 
-# testthat::test_that('covrpage travis',{
-# 
-#   if(Sys.getenv("CI") == "true" && Sys.getenv("TRAVIS") == "true"){
-#     
-#     setwd('/home/travis/build/yonicd/covrpage')
-#     
-#     covrpage::covrpage('.',preview = FALSE)
-#     
-#     system('git add tests/README.md')
-#     system('git commit -m "update tests readme [skip ci]"')
-#     system('git push')
-#     
-#   }
-# 
-#   expect_true(2+2==4)
-# })
+testthat::test_that('covrpage travis',{
+
+  if(Sys.getenv("CI") == "true" && Sys.getenv("TRAVIS") == "true"){
+
+    #covrpage::covrpage('.',preview = FALSE)
+    
+    setwd('/home/travis/build/yonicd/covrpage')
+    system('git config --global user.email "travis@travis-ci.org"')
+    system('git config --global user.name "Travis CI"')
+    file.create('test.txt')
+    system('git add test.txt')
+    system('git commit -m "try a test [skip ci]"')
+    system('git push')
+
+  }
+
+  expect_true(2+2==4)
+})
