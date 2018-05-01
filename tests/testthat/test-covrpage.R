39fbc6fad427e3f871a9246458458a3211a20bc4
Jonathan Sidi <yonicd@gmail.com>
2018-04-30 23:37:41 -0400

    update test

--- tests/testthat/test-covrpage.R
+++ tests/testthat/test-covrpage.R
@@ -26,11 +26,14 @@ test_that("check on remote repo",{
 #   expect_null(covrpage_snapshot(repo = 'hrbrmstr/slackr',preview = FALSE))
 # })
 
-thiswd <- getwd()
+td <- getwd()
 
 testthat::test_that('covrpage travis',{
-  if(grepl('travis',normalizePath(dir(full.names = TRUE)[1])))
-    covrpage::covrpage(pkg = thiswd, preview = FALSE, auto_push = TRUE)
   
+  if(grepl('travis',thiswd)){
+    setwd(thiswd)
+    covrpage::covrpage(pkg = '.', preview = FALSE, auto_push = TRUE)
+  }
+    
   expect_true(2+2==4)
 })
