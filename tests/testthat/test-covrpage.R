b8f0f621bad11901e6d075f05d7bf1174f1d0dd6
Jonathan Sidi <yonicd@gmail.com>
2018-05-01 23:23:15 -0400

    update travis test

--- tests/testthat/test-covrpage.R
+++ tests/testthat/test-covrpage.R
@@ -39,27 +39,23 @@ test_that("check on remote repo",{
 # })
 
 
-testthat::test_that('covrpage travis',{
-
-  if(Sys.getenv("CI") == "true" && Sys.getenv("TRAVIS") == "true"){
-
-    #covrpage::covrpage('.',preview = FALSE)
-    
-    setwd('/home/travis/build/yonicd/covrpage')
-    system('git config --global user.email "yonicd@gmail.com"')
-    system('git config --global user.name "yonicd"')
-    system('git checkout -b covrpage')
-    
-    file.create('test.txt')
-    
-    system('git add test.txt')
-    
-    system('git remote add master-covrpages https://${GH_TOKEN}@github.com/yonicd/covrpage.git')
-    
-    system('git commit -m "try a test [skip ci]"')
-    system('git push --quiet --set-upstream master covrpage ')
-
-  }
-
-  expect_true(2+2==4)
-})
+if(Sys.getenv("CI") == "true" && Sys.getenv("TRAVIS") == "true"){
+  
+  thiswd <- getwd()
+  
+  setwd('/home/travis/build/yonicd/covrpage')
+  system('git config --global user.email "yonicd@gmail.com"')
+  system('git config --global user.name "yonicd"')
+  system('git checkout -b covrpage')
+  
+  file.create('test.txt')
+  
+  system('git add test.txt')
+  
+  system('git remote add master-covrpages https://${GH_TOKEN}@github.com/yonicd/covrpage.git')
+  
+  system('git commit -m "try a test [skip ci]"')
+  system('git push --quiet --set-upstream master covrpage ')
+  
+  setwd(thiswd)
+}
