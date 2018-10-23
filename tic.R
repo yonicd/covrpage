b072a19b272156203cb4615a482f2575f867d502
Jonathan Sidi <yonis@metrumrg.com>
2018-10-22 22:19:44 -0400

    update tic.R [skip travis]

--- tic.R
+++ tic.R
@@ -16,28 +16,28 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
     ) %>%
     add_code_step(devtools::update_packages(TRUE))
   
-  if (inherits(ci(), "TravisCI")) {
-    get_stage("before_script") %>%
-      add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
-                                             "-install-package", "thirdparty/XMeans1.0.4.zip")))
-  }
+  # if (inherits(ci(), "TravisCI")) {
+  #   get_stage("before_script") %>%
+  #     add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
+  #                                            "-install-package", "thirdparty/XMeans1.0.4.zip")))
+  # }
   
-  get_stage("script") %>%
-    add_code_step(devtools::document()) %>%
-    # manual approch until https://github.com/r-lib/rcmdcheck/issues/83#issuecomment-424314978 is solved
-    add_code_step(devtools::build(manual = TRUE)) %>%
-    add_step(step_rcmdcheck(path = "../mlr_2.13.9000.tar.gz", args = "--as-cran",
-                            error_on = "error"))
+  # get_stage("script") %>%
+  #   add_code_step(devtools::document()) %>%
+  #   # manual approch until https://github.com/r-lib/rcmdcheck/issues/83#issuecomment-424314978 is solved
+  #   add_code_step(devtools::build(manual = TRUE)) %>%
+  #   add_step(step_rcmdcheck(path = "../mlr_2.13.9000.tar.gz", args = "--as-cran",
+  #                           error_on = "error"))
   
-  if (!Sys.getenv("TRAVIS_EVENT_TYPE") == "cron") {
-    
-    get_stage("before_deploy") %>%
-      add_step(step_setup_ssh())
-    
-    get_stage("deploy") %>%
-      add_code_step(devtools::document()) %>%
-      add_step(step_push_deploy(commit_paths = "man/"))
-  }
+  # if (!Sys.getenv("TRAVIS_EVENT_TYPE") == "cron") {
+  #   
+  #   get_stage("before_deploy") %>%
+  #     add_step(step_setup_ssh())
+  #   
+  #   get_stage("deploy") %>%
+  #     add_code_step(devtools::document()) %>%
+  #     add_step(step_push_deploy(commit_paths = "man/"))
+  # }
 }
 
 if (Sys.getenv("TUTORIAL") == "HTML") {
@@ -59,7 +59,7 @@ if (Sys.getenv("TUTORIAL") == "HTML") {
     
     get_stage("deploy") %>%
       add_step(step_build_pkgdown()) #%>%
-    #add_step(step_push_deploy(commit_paths = "docs/*"))
+      add_step(step_push_deploy(commit_paths = "docs/*"))
     
   }
 }
