635d0f1b3fc663fc13fe79a40812e8fd5afb5fbd
Jonathan Sidi <yonis@metrumrg.com>
2018-10-24 22:09:38 -0400

    run deploy manually [skip travis]

--- tic.R
+++ tic.R
@@ -5,8 +5,7 @@ get_stage("after_script") %>%
 
 get_stage("deploy") %>%
   add_step(step_build_pkgdown())%>%
-  add_step(step_setup_push_deploy(branch = "gh-pages",orphan = TRUE))%>%
-  add_step(step_do_push_deploy(commit_paths = "docs/*"))
+  add_step(step_setup_push_deploy(branch = "gh-pages",orphan = TRUE))
 
 get_stage("after_success") %>%
   add_code_step(setwd('c:/projects/covrpage'))%>%
