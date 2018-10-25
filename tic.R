7e13c81eb59ed3bb212cbd0ceb79dcf103f2b9cd
Jonathan Sidi <yonis@metrumrg.com>
2018-10-24 22:53:23 -0400

    do before_deploy [skip travis]

--- tic.R
+++ tic.R
@@ -3,7 +3,7 @@
 get_stage("after_script") %>%
   add_code_step(system("rm -rf $HOME/R/Library/00LOCK-*"))
 
-get_stage("deploy") %>%
+get_stage("before_deploy") %>%
   add_step(step_build_pkgdown())%>%
   add_step(step_setup_push_deploy(branch = "test",orphan = TRUE))%>%
   add_step(step_do_push_deploy(path = 'docs'))
