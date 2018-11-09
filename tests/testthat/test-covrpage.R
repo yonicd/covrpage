c53adaef20822a5c2e255e13678235e9c70633f6
Jonathan Sidi <yonis@metrumrg.com>
2018-11-08 22:29:58 -0500

    skip covrpage_test on travis

--- tests/testthat/test-covrpage.R
+++ tests/testthat/test-covrpage.R
@@ -2,6 +2,8 @@ testthat::context("check against remote repo that main function is working")
 
 testthat::describe("check on remote repo", {
 
+  testthat::skip_on_travis()
+  
   file.copy("../assets/covrpage_benchmark/covrpage",tempdir(),recursive = TRUE)
 
   td <- file.path(tempdir(),'covrpage')
