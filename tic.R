5483e3f11fa52e05d3b5e7232f5427d57baa4560
Jonathan Sidi <yonis@metrumrg.com>
2018-11-20 22:25:04 -0500

    test tic

---
+++ tic.R
@@ -0,0 +1,28 @@
+add_package_checks()
+
+if (Sys.getenv("id_rsa") != "") {
+  # pkgdown documentation can be built optionally. Other example criteria:
+  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
+  # - `ci()$is_tag()`: Only for tags, not for branches
+  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
+  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
+  get_stage("before_deploy") %>%
+    add_step(step_setup_ssh())
+  
+  if (ci()$get_branch() == "tic") {
+    get_stage("deploy") %>%
+      add_code_step(covr::codecov()) %>%
+      add_code_step(devtools::install()) %>%
+      add_code_step(install.packages("spelling")) %>%
+      add_code_step(covrpage::covrpage_ci()) %>%
+      add_step(step_push_deploy(commit_paths = "tests/README.md")) %>%
+      add_step(step_build_pkgdown()) %>%
+      add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+  }else{
+    get_stage("deploy") %>%
+      add_code_step(covr::codecov()) %>%
+      add_code_step(devtools::install()) %>%
+      add_code_step(covrpage::covrpage_ci()) %>%
+      add_step(step_push_deploy(commit_paths = "tests/README.md"))
+  }
+}
