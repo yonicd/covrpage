87cb2100f706061ce94fae626f0e1ea8a695da8e
Jonathan Sidi <yonis@metrumrg.com>
2018-11-27 21:42:52 -0500

    skip utilities test on travis

--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -1,7 +1,7 @@
 testthat::context('utils')
 
 testthat::describe('find package in path',{
-  
+  testthat::skip_on_travis()
   file.copy("../assets/covrpage_benchmark/covrpage", tempdir(), recursive = TRUE)
   td <- file.path(tempdir(), "covrpage")
   
