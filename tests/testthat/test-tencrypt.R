f55c021263c0cf46f7474aad533e10e6b1e184ff
Yoni Sidi <yoni.sidi@sagerx.com>
2022-12-22 14:35:12 -0500

    remove travis and appveyor

--- tests/testthat/test-tencrypt.R
+++ tests/testthat/test-tencrypt.R
@@ -1,27 +1,27 @@
-testthat::context("encryption of PAT")
-
-testthat::describe("testing tencrypt", {
-  testthat::skip_on_ci()
-
-  file.copy("../assets/covrpage_benchmark/covrpage", tempdir(), recursive = TRUE)
-  td <- file.path(tempdir(), "covrpage")
-  wd <- getwd()
-  setwd(td)
-  repo <- git2r::init()
-  file.create(".travis.yml")
-  git2r::add(repo, ".travis.yml")
-  git2r::remote_add(repo, name = "origin", url = "git@github.com:yonicd/covrpage.git")
-
-  it("no add", {
-    testthat::expect_equal(nchar(tencrypt(add = FALSE)), 686)
-  })
-
-  tencrypt()
-
-  it("with add", {
-    testthat::expect_equal(length(readLines(".travis.yml")), 3)
-  })
-
-  setwd(wd)
-  unlink(td, recursive = TRUE, force = TRUE)
-})
+# testthat::context("encryption of PAT")
+# 
+# testthat::describe("testing tencrypt", {
+#   testthat::skip_on_ci()
+# 
+#   file.copy("../assets/covrpage_benchmark/covrpage", tempdir(), recursive = TRUE)
+#   td <- file.path(tempdir(), "covrpage")
+#   wd <- getwd()
+#   setwd(td)
+#   repo <- git2r::init()
+#   file.create(".travis.yml")
+#   git2r::add(repo, ".travis.yml")
+#   git2r::remote_add(repo, name = "origin", url = "git@github.com:yonicd/covrpage.git")
+# 
+#   it("no add", {
+#     testthat::expect_equal(nchar(tencrypt(add = FALSE)), 686)
+#   })
+# 
+#   tencrypt()
+# 
+#   it("with add", {
+#     testthat::expect_equal(length(readLines(".travis.yml")), 3)
+#   })
+# 
+#   setwd(wd)
+#   unlink(td, recursive = TRUE, force = TRUE)
+# })
